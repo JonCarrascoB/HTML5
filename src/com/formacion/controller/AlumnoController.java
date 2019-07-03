4358955cc811b20d1a3ad8a1e4ca15e0222b998f
Jon Carrasco <jcarrascobar@gmail.com>
2019-07-03 18:08:34 +0200

    commit crear alumno

---
+++ src/com/formacion/controller/AlumnoController.java
@@ -0,0 +1,64 @@
+package com.formacion.controller;
+
+import java.io.IOException;
+import java.util.ArrayList;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.formacion.modelo.pojo.Alumno;
+
+/**
+ * Servlet implementation class AlumnoController
+ */
+@WebServlet("/alumnos")
+public class AlumnoController extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+    private ArrayList<Alumno> alumnos = new ArrayList<Alumno>();
+    
+    private static final String VIEW_FORMULARIO = "/alumno/formulario-alumno.jsp";
+	private static final String VIEW_LISTADO = "/alumno/listado-alumnos.jsp";
+	static String view = VIEW_LISTADO;
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doPost(request, response);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		String nombre = request.getParameter("nombre");
+		String edad = request.getParameter("edad");
+		String altura = request.getParameter("altura");
+		String peso = request.getParameter("peso");
+		String ojos = request.getParameter("ojos");
+		String sexo = request.getParameter("sexo");
+		
+		//validar los parametro - no implementar
+		
+		//Crear el alumno
+		Alumno alumno = new Alumno();
+		alumno.setNombre(nombre);
+		alumno.setEdad(Integer.parseInt(edad));
+		alumno.setAltura(Float.parseFloat(altura));
+		alumno.setPeso(Float.parseFloat(peso));
+		alumno.setOjos(ojos);
+		alumno.setSexo(sexo.charAt(0));
+		
+		request.setAttribute("alumno", alumno);
+		alumnos.add(alumno);
+		request.setAttribute("alumnos", alumnos);
+		
+		// request interna o ir a una JSP
+		request.getRequestDispatcher(view).forward(request, response);
+	}
+
+}
---
+++ src/com/formacion/modelo/pojo/Alumno.java
@@ -0,0 +1,57 @@
+package com.formacion.modelo.pojo;
+
+import java.io.Serializable;
+
+public class Alumno extends Person implements Comparable<Alumno>, Serializable {
+	
+	private int numVecesLeer;
+
+	public Alumno(int id, String nombre, int numVecesLeer) {
+		super(nombre);
+		super.setId(id);
+		this.setNumVecesLeer (numVecesLeer);
+	}
+	
+	public Alumno(String nombre, int numVecesLeer) {
+		super(nombre);
+		this.setNumVecesLeer (numVecesLeer);
+	}
+	
+	public Alumno(int id, String nombre) {
+		super(nombre);
+		super.setId(id);
+		this.numVecesLeer = 0;
+	}
+	
+	public Alumno( String nombre) {
+		super(nombre);
+		this.numVecesLeer = 0;
+	}
+	public Alumno() {
+		
+	}
+	
+	public int getNumVecesLeer() {
+		return numVecesLeer;
+	}
+
+	public void setNumVecesLeer(int numVecesLeer) {
+		this.numVecesLeer = numVecesLeer;
+	}
+	
+	
+
+	@Override
+	public String toString() {
+		return "Alumno [numVecesLeer=" + numVecesLeer + "]";
+	}
+
+	@Override
+	public int compareTo(Alumno o) {
+		return  o.numVecesLeer - this.getNumVecesLeer();
+	}
+	
+	
+	
+
+}
